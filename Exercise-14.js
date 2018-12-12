2fb8d90d263f28b60e3c5cc9af14864367f5b7e9
Arnov <arnov.julian17@gmail.com>
2018-12-12 18:50:06 +0700

    perbaikan

--- Exercise-14.js
+++ Exercise-14.js
@@ -1,18 +1,31 @@
 function changeVocals(str) {
-  var hurufAwal = ['a', 'i', 'u', 'e', 'o', 'A', 'I', 'U', 'E', 'O'];
-  var hurufUbah = ['b', 'j', 'v', 'f', 'p', 'B', 'J', 'V', 'F', 'P'];
-  var temp = ''
-  for (var i = 0; i < str.length; i++) {
-    for (var j = 0; j < hurufAwal.length; j++) {
-      if (str[i] === hurufAwal[j]) {
-        temp = temp + hurufUbah[j]
-      }
-    }
-    if (i === (temp.length)) {
-      temp = temp + str[i]
+  var result = ''
+  for(var i = 0; i < str.length; i++) {
+    if(str[i] === 'a') {
+      result += 'b'
+    } else if(str[i] === 'i') {
+      result += 'j'
+    } else if(str[i] === 'u') {
+      result += 'v'
+    } else if(str[i] === 'e') {
+      result += 'f'
+    }  else if(str[i] === 'o') {
+      result += 'p'
+    } else if(str[i] === 'A') {
+      result += 'B'
+    } else if(str[i] === 'I') {
+      result += 'J'
+    } else if(str[i] === 'U') {
+      result += 'V'
+    } else if(str[i] === 'E') {
+      result += 'F'
+    } else if(str[i] === 'O') {
+      result += 'P'
+    } else {
+      result += str[i]
     }
   }
-  return temp
+  return result
 
 }
 
