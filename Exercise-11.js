1780d89c038b8237e09eb7f7ce78989a732303de
Arnov <arnov.julian17@gmail.com>
2018-12-13 16:10:24 +0700

    perbaikan di money, jika ada 50000

--- Exercise-11.js
+++ Exercise-11.js
@@ -18,7 +18,7 @@ function shoppingTime(memberId, money) {
     var totalHarga = 0
     for (var i = 0; i < barang.length; i++) {
 
-        if (money > barang[i][1]) {
+        if (money >= barang[i][1]) {
             result.listPurchased.push(barang[i][0])
             totalHarga += barang[i][1]
         }
@@ -31,7 +31,7 @@ function shoppingTime(memberId, money) {
         return "Mohon maaf, toko X hanya berlaku untuk member saja"
         }
 
-    if (money <= 50000) {
+    if (money < 50000) {
         return 'Mohon maaf, uang tidak cukup'
         }
 
@@ -63,4 +63,3 @@ console.log(shoppingTime('82Ku8Ma742', 170000));
 console.log(shoppingTime('', 2475000)); //Mohon maaf, toko X hanya berlaku untuk member saja
 console.log(shoppingTime('234JdhweRxa53', 15000)); //Mohon maaf, uang tidak cukup
 console.log(shoppingTime()); ////Mohon maaf, toko X hanya berlaku untuk member saja
-
